b5b6b3ed37e1818520d66531fa2ffd28be5124b5
yumiaoxia <yumiaoxia@163.com>
2019-09-07 14:21:00 +0800

    参数记录

--- common-web/src/main/java/com/itsherman/commonweb/exception/ExceptionController.java
+++ common-web/src/main/java/com/itsherman/commonweb/exception/ExceptionController.java
@@ -1,11 +1,11 @@
 package com.itsherman.commonweb.exception;
 
+import com.itsherman.commonweb.request.RequestWrapper;
 import com.itsherman.commonweb.response.ApiResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.ServletInputStream;
@@ -15,6 +15,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
+import java.util.Map;
 
 /**
  * <p> </p>
@@ -24,39 +25,43 @@ import java.nio.charset.Charset;
  */
 @ControllerAdvice
 public class ExceptionController {
-
+    private static final String APPLICATION_JSON = "application/json";
     private static final Logger log = LoggerFactory.getLogger(ExceptionController.class);
 
     @ResponseBody
     @ExceptionHandler(Exception.class)
     public ApiResponse handleException(HttpServletRequest request, Exception ex) {
-        log.error("Exception Occurred: {}, requestURL: {},params: {}", ex.getMessage(), request.getRequestURL(), getRequestParams(request));
-        log.error(ex.getMessage(), ex);
+        log.error("Exception Occurred! requestURL: {}, params: {}", request.getRequestURL(), getRequestParams(request), ex);
         return ApiResponse.createError();
     }
 
 
-    private String getRequestParams(HttpServletRequest request) {
-        String result = "";
-        if (RequestMethod.GET.name().equals(request.getMethod())) {
-            result = request.getQueryString();
-        } else if (RequestMethod.POST.name().equals(request.getMethod()) || RequestMethod.PUT.name().equals(request.getMethod())) {
-            HttpServletRequestWrapper requestWrapper = new HttpServletRequestWrapper(request);
+    protected String getRequestParams(HttpServletRequest request) {
+        StringBuilder result = new StringBuilder();
+        if (request.getContentType().equals(APPLICATION_JSON)) {
+            HttpServletRequestWrapper requestWrapper = new RequestWrapper(request);
+            ServletInputStream inputStream = null;
+            InputStreamReader reader = null;
+            BufferedReader bufferedReader = null;
             try {
-                ServletInputStream inputStream = requestWrapper.getInputStream();
-                InputStreamReader streamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
-                BufferedReader bufferedReader = new BufferedReader(streamReader);
+                inputStream = requestWrapper.getInputStream();
+                reader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+                bufferedReader = new BufferedReader(reader);
                 String line = "";
-                StringBuilder sb = new StringBuilder(line);
+                StringBuilder sb = new StringBuilder("requestBody: ");
                 while ((line = bufferedReader.readLine()) != null) {
                     sb.append(line);
                 }
-                result = sb.toString();
+                result = result.append(sb);
+                bufferedReader.close();
+                reader.close();
+                inputStream.close();
             } catch (IOException e) {
-                e.printStackTrace();
-                return result;
+                log.error(e.getMessage(), e);
             }
         }
-        return result;
+        Map<String, String[]> parameterMap = request.getParameterMap();
+        result = result.append(parameterMap);
+        return result.toString();
     }
 }
--- common-web/src/main/java/com/itsherman/commonweb/locale/BundleMessageProperties.java
+++ common-web/src/main/java/com/itsherman/commonweb/locale/BundleMessageProperties.java
@@ -2,7 +2,6 @@ package com.itsherman.commonweb.locale;
 
 import org.springframework.boot.autoconfigure.context.MessageSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
 import org.springframework.stereotype.Component;
 
 /**
@@ -12,7 +11,6 @@ import org.springframework.stereotype.Component;
  * @since 2019-09-06
  */
 @Component
-@ConfigurationPropertiesBinding
 @ConfigurationProperties(prefix = "spring.messages")
 public class BundleMessageProperties extends MessageSourceProperties {
 }
